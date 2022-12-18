827ebef9799247c16fc20fec9dd5955a1c639415
Konotorii <konotoribusiness@gmail.com>
2022-12-17 21:46:24 -0500

    Initial commit

---
+++ scripts/yt-mp3-thumb.py
@@ -0,0 +1,9 @@
+import os
+
+file = open('music-mp3.txt', 'r')
+lines = file.readlines()
+
+count = 0
+for line in lines:
+    count += 1
+    os.system(f"youtube-dl --format bestaudio -x --audio-format mp3 --embed-thumbnail {line}")
